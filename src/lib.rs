0bc1cb12e6ce2f496f236bf2b4bf72927e876bde
PetrStudynka <studynkapetr@gmail.com>
2020-12-12 09:57:49 +0100

    todo command refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,32 @@ pub fn copy_dir_entry(cmd: &Command) -> Result<String, String> {
     Err("Not implemented yet.".to_string())
 }
 
-/// Removes file/dir
+/// Removes dir entry
 /// Option -f for recursive option in case of dir
 /// Returns Err in case of insuffient rights/unexisting entry
-/// rm <options> <file/dir>
+/// rm <options> <dir entry>
 pub fn remove_dir_entry(cmd: &Command) -> Result<String, String> {
+
+    /*
+    let file = cmd.get_parameter_at(cmd.get_params_count()-1);
+
+    let options = cmd.get_parameter_at(cmd.get_params_count()-2);
+
+    let options = match options {
+        Ok("-r") => Some(_),
+        Ok(_) => None,
+        Err(_) => panic!("Invalid state"),
+    };
+
+    if file == cmd.get_name(){
+        return Err("Missing parameter.")
+    }
+
+    let file = match file {
+        Ok(s) => s,
+        Err(_) => return Err("Entry param is not specified".to_string()),
+    };
+
     if cmd.get_params_count() > 1 {
         let options = cmd.get_parameter_at(0);
         let file = cmd.get_parameter_at(1);
@@ -24,13 +45,7 @@ pub fn remove_dir_entry(cmd: &Command) -> Result<String, String> {
 
         let metadata = match metadata(file) {
             Ok(r) => r,
-            Err(e) if e.kind() == ErrorKind::PermissionDenied => {
-                return Err("Permission denied".to_string())
-            }
-            Err(e) if e.kind() == ErrorKind::NotFound => {
-                return Err(format!("{} does not exists", file))
-            }
-            _ => return Err("Unspecified error".to_string()),
+            Err(e) => return Err(e.to_string()),
         };
 
         if metadata.is_file() {
@@ -61,50 +76,88 @@ pub fn remove_dir_entry(cmd: &Command) -> Result<String, String> {
         };
 
         rm_file(file)
+
     }
+    */
+    Ok("ok".to_string())
 }
 
 fn rm_file(file: &str) -> Result<String, String> {
     match remove_file(file) {
         Ok(()) => return Ok(format!("File: {} deleted", file)),
-        Err(e) if e.kind() == ErrorKind::PermissionDenied => {
-            return Err("Permission denied".to_string())
-        }
-        Err(e) if e.kind() == ErrorKind::NotFound => return Err(format!("{} does not exists", file)),
-        _ => return Err("Unspecified error".to_string()),
+        Err(e) => return Err(e.to_string()),
     };
 }
 
+/// <command> <options> <arguments...>
 pub struct Command<'a> {
     name: String,
-    params: Vec<&'a str>,
+    options: Option<Vec<char>>,
+    params: Option<Vec<&'a str>>,
 }
 
 impl Command<'_> {
-    pub fn new(params: Vec<&str>) -> Command {
-        if params.len() < 1 {
-            panic!("Vector must not be empty.")
+    pub fn new(buffer: Vec<&str>) -> Command {
+        if buffer.len() < 1 || buffer[0].len() < 1 {
+            panic!("Command name must be specified.")
         }
 
+        let (pars,opts) = match buffer.len() > 1 {
+           true => if buffer.len() > 2 {
+               let mut chars = buffer[1].chars().peekable();
+            if chars.next() == Some('-') && chars.peek() != None {
+                (Some(buffer[2..].to_vec()), Some(chars.collect::<Vec<_>>()))
+            } else {
+                 (Some(buffer[1..].to_vec()), None)
+            }
+           } else {
+            (Some(buffer[1..].to_vec()),None)
+           }
+           false => {
+               (None,None)
+           }
+        };
+
         Command {
-            name: params[0].to_string(),
-            params: params[1..].to_vec(),
+            name: buffer[0].to_string(),
+            options: opts,
+            params: pars,
         }
     }
 
+    
     pub fn get_name(&self) -> &str {
         &self.name
     }
 
-    pub fn get_parameter_at(&self, index: usize) -> Result<&str, &'static str> {
-        match self.params.len() > index {
-            true => Ok(&self.params[index]),
-            false => Err("Index out of bound"),
+    pub fn get_parameter_at(&self, index: usize) -> Option<&str> {
+        match &self.params {
+            Some(v) =>  {
+                if v.len() > index {
+                    Some(&v[index])
+                }
+                else {
+                    None
+                }
+            }
+            None => None
         }
     }
 
+    /*
+    pub fn get_options(&self, index: usize) -> Result<&str, &'static str> {
+        match self.params.len() > index {
+            true => Ok(&self.params[index]),
+            false => None,
+        }
+    }
+    */
+
     pub fn get_params_count(&self) -> usize {
-        self.params.len()
+        match &self.params {
+            Some(v) => v.len(),
+            None => 0,
+        }
     }
 }
 
